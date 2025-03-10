993cdd4467be4303ae7df4b9e3dba8e95ee6a632
Sergey Dribas <serdribas@gmail.com>
2025-03-10 11:25:30 +0300

    release find function

--- dribas.sergey/K5/main.cpp
+++ dribas.sergey/K5/main.cpp
@@ -1,3 +1,22 @@
+template< class T >
+struct BiTree {
+  T data;
+  BiTree< T > * left, * right;
+};
+
+template< class T, class Cmp >
+const BiTree< T > * find(const BiTree< T >* root, const T & value, Cmp cmp)
+{
+  if (root == nullptr || !cmp(root->data, value) && !cmp(value, root->data)) {
+    return root;
+  }
+  if (cmp(value, root->data)) {
+    return find(root->left, value, cmp);
+  } else {
+    return find(root->right, value, cmp);
+  }
+}
+
 int main()
 {
   return 0;
