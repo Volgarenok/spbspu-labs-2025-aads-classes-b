635cbe7e6c8fa2720a608c57cbaa81be70751cc2
Robotophile <ksyunya.iv@gmail.com>
2025-03-24 12:10:30 +0300

    iterator:

--- ivanova.ksenia/K7/main.cpp
+++ ivanova.ksenia/K7/main.cpp
@@ -1,24 +1,72 @@
+#include <iostream>
+
 template< class T, class Cmp >
-struct BiTree {
+struct BiTree
+{
   T data;
   Cmp cmp;
   BiTree< T > * left, * right, * parent;
 };
 
 template< class T >
-struct BiTreeIterator {
+struct BiTreeIterator
+{
   BiTree< T > * node;
 
-  bool hasPrev() const;
-  bool hasNext() const;
-  BiTreeIterator< T > next() const;
-  BiTreeIterator< T > prev() const;
+  bool hasPrev() const
+  {
+    if(!node)
+    {
+      return false;
+    }
+    return node->left;
+  }
+  bool hasNext() const
+  {
+    if(!node)
+    {
+      return false;
+    }
+    return node->right;
+  }
+  BiTreeIterator< T > next() const
+  {
+    if (!node || !node->right)
+    {
+      std::cerr << "No previous element" << '\n';
+      throw;
+    }
+    return BiTreeIterator<T, Cmp>(node->right);
+  }
+  BiTreeIterator< T > prev() const
+  {
+    if (!node || !node->left)
+    {
+      std::cerr << "No previous element" << '\n';
+      throw;
+    }
+    return BiTreeIterator<T, Cmp>(node->left);
+  }
 
-  const T & data() const;
+  const T & data() const
+  {
+    if (!node)
+    {
+      std::cerr << "Iterator is not dereferenceable" << '\n';
+      throw;
+    }
+    return node->data;
+  }
 };
 
 int main()
 {
+  int n;
+  std::cin >> n;
+  if (n < 0) {
+    std::cerr << "Invalid sequence length" << '\n';
+    return 1;
+  }
   //в твоём сообщении много букв
   //значит мне уже пофиг
 }
