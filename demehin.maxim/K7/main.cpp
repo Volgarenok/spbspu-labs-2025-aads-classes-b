b15f18ec24202436269ad9cf55d0bcde70a903b7
TvoyBatyA12343 <maxdem06@gmail.com>
2025-03-24 14:25:40 +0300

    done(?)

--- demehin.maxim/K7/main.cpp
+++ demehin.maxim/K7/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <functional>
+#include <string>
 
 template< class T, class Cmp >
 struct BiTree
@@ -14,13 +15,104 @@ struct BiTreeIterator
 {
   BiTree< T, Cmp >* node;
 
-  bool hasPrev() const;
-  bool hasNext() const;
-  BiTreeIterator< T, Cmp > next() const;
-  BiTreeIterator< T, Cmp > prev() const;
+  bool hasPrev() const
+  {
+    if (node == nullptr)
+    {
+      return false;
+    }
+    if (node->left != nullptr)
+    {
+      return true;
+    }
 
-  const T& data() const;
+    BiTree< T, Cmp >* current = node;
+    while (current->parent != nullptr && current == current->parent->left)
+    {
+      current = current->parent;
+    }
+    return current->parent != nullptr;
+  }
+
+  bool hasNext() const
+  {
+    if (node == nullptr)
+    {
+      return false;
+    }
+    if (node->right != nullptr)
+    {
+      return true;
+    }
+
+    BiTree< T, Cmp >* current = node;
+    while (current->parent != nullptr && current == current->parent->right)
+    {
+      current = current->parent;
+    }
+    return current->parent != nullptr;
+
+  }
+
+  BiTreeIterator< T, Cmp > next() const
+  {
+    if (node == nullptr)
+    {
+      return {nullptr};
+    }
+
+    if (node->right)
+    {
+      BiTree< T, Cmp >* current = node->right;
+      while (current->left != nullptr)
+      {
+        current = current->left;
+      }
+      return {current};
+    }
+    else
+    {
+      BiTree< T, Cmp >* current = node;
+      while (current->parent != nullptr && current == current->parent->right)
+      {
+        current = current->parent;
+      }
+      return {current->parent};
+    }
+  }
+
+  BiTreeIterator< T, Cmp > prev() const
+  {
+    if (node == nullptr)
+    {
+      return {nullptr};
+    }
+    if (node->left != nullptr)
+    {
+      BiTree< T, Cmp >* current = node->left;
+      while (current->right != nullptr)
+      {
+        current = current->right;
+      }
+      return {current};
+    }
+    else
+    {
+      BiTree< T, Cmp >* current = node;
+      while (current->parent != nullptr && current == current->parent->left)
+      {
+        current = current->parent;
+      }
+      return {current->parent};
+    }
+  }
+
+  const T& data() const
+  {
+    return node->data;
+  }
 };
+
 
 template< class T, class Cmp >
 BiTree< T, Cmp >* insNode(BiTree< T, Cmp >* root, const T& value, Cmp cmp, BiTree< T, Cmp >* parent = nullptr)
@@ -54,6 +146,38 @@ void clear(BiTree< T, Cmp >* root)
   delete root;
 }
 
+template< class T, class Cmp >
+void printTomax(BiTree< T, Cmp >* root, std::ostream& out)
+{
+  BiTreeIterator< T, Cmp > minIt{root};
+  while (minIt.hasPrev())
+  {
+    minIt = minIt.prev();
+  }
+  out << minIt.data();
+  while (minIt.hasNext())
+  {
+    minIt = minIt.next();
+    out << "\n" << minIt.data();
+  }
+}
+
+template< class T, class Cmp >
+void printTomin(BiTree< T, Cmp >* root, std::ostream& out)
+{
+  BiTreeIterator< T, Cmp > maxIt{root};
+  while (maxIt.hasNext())
+  {
+    maxIt = maxIt.next();
+  }
+  out << maxIt.data();
+  while (maxIt.hasPrev())
+  {
+    maxIt = maxIt.prev();
+    out << "\n" << maxIt.data();
+  }
+}
+
 int main()
 {
   size_t n = 0;
@@ -69,6 +193,30 @@ int main()
       return 1;
     }
     root = insNode(root, num, std::less< int >(), root);
+  }
+  std::string comand;
+  if (!(std::cin >> comand))
+  {
+    std::cerr << "input error\n";
+    clear(root);
+    return 1;
+  }
+
+  if (comand == "tomax")
+  {
+    printTomax(root, std::cout);
+    std::cout << "\n";
+  }
+  else if (comand == "tomin")
+  {
+    printTomin(root, std::cout);
+    std::cout << "\n";
+  }
+  else
+  {
+    std::cerr << "wrong comand\n";
+    clear(root);
+    return 1;
   }
 
   clear(root);
