700fd48d08d64654861397bd85a496eee8f24611
TvoyBatyA12343 <maxdem06@gmail.com>
2025-03-24 12:11:23 +0300

    insertion impl

--- demehin.maxim/K7/main.cpp
+++ demehin.maxim/K7/main.cpp
@@ -1,23 +1,75 @@
+#include <iostream>
+#include <functional>
+
 template< class T, class Cmp >
 struct BiTree
 {
   T data;
   Cmp cpm;
-  BiTree< T >* left, * right, * parent;
+  BiTree< T, Cmp >* left, * right, * parent;
 };
 
-template< class T >
+template< class T, class Cmp >
 struct BiTreeIterator
 {
-  BiTree< T >* node;
+  BiTree< T, Cmp >* node;
 
   bool hasPrev() const;
   bool hasNext() const;
-  BiTreeIterator< T > next() const;
-  BiTreeIterator< T > prev() const;
+  BiTreeIterator< T, Cmp > next() const;
+  BiTreeIterator< T, Cmp > prev() const;
 
   const T& data() const;
 };
 
+template< class T, class Cmp >
+BiTree< T, Cmp >* insNode(BiTree< T, Cmp >* root, const T& value, Cmp cmp, BiTree< T, Cmp >* parent = nullptr)
+{
+  if (root == nullptr)
+  {
+    BiTree< T, Cmp >* newNode = new BiTree< T, Cmp >{value, cmp, nullptr, nullptr, parent};
+    return newNode;
+  }
+
+  if (cmp(value, root->data))
+  {
+    root->left = insNode(root->left, value, cmp, root);
+  }
+  else
+  {
+    root->right = insNode(root->right, value, cmp, root);
+  }
+  return root;
+}
+
+template< class T, class Cmp >
+void clear(BiTree< T, Cmp >* root)
+{
+  if (root == nullptr)
+  {
+    return;
+  }
+  clear(root->left);
+  clear(root->right);
+  delete root;
+}
+
 int main()
-{}
+{
+  size_t n = 0;
+  std::cin >> n;
+  BiTree< int, std::less< int > >* root = nullptr;
+  int num = 0;
+  for (size_t i = 0; i < n; i++)
+  {
+    if (!(std::cin >> num))
+    {
+      std::cerr << "input error\n";
+      clear(root);
+      return 1;
+    }
+    root = insNode(root, num, std::less< int >(), root);
+  }
+
+  clear(root);
+}
