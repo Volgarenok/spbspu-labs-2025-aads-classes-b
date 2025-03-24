b17183b69259b8d459d39bbdfe5c3c9a5c608afa
TvoyBatyA12343 <maxdem06@gmail.com>
2025-03-24 11:28:28 +0300

    structs

--- demehin.maxim/K7/main.cpp
+++ demehin.maxim/K7/main.cpp
@@ -1,2 +1,23 @@
+template< class T, class Cmp >
+struct BiTree
+{
+  T data;
+  Cmp cpm;
+  BiTree< T >* left, * right, * parent;
+};
+
+template< class T >
+struct BiTreeIterator
+{
+  BiTree< T >* node;
+
+  bool hasPrev() const;
+  bool hasNext() const;
+  BiTreeIterator< T > next() const;
+  BiTreeIterator< T > prev() const;
+
+  const T& data() const;
+};
+
 int main()
 {}
