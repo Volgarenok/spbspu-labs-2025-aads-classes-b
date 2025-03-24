9fcfb4a793dc9dfec2fca93b002266cdf46f3fdb
Robotophile <ksyunya.iv@gmail.com>
2025-03-24 11:36:53 +0300

    shablon

--- ivanova.ksenia/K7/main.cpp
+++ ivanova.ksenia/K7/main.cpp
@@ -1,3 +1,22 @@
+template< class T, class Cmp >
+struct BiTree {
+  T data;
+  Cmp cmp;
+  BiTree< T > * left, * right, * parent;
+};
+
+template< class T >
+struct BiTreeIterator {
+  BiTree< T > * node;
+
+  bool hasPrev() const;
+  bool hasNext() const;
+  BiTreeIterator< T > next() const;
+  BiTreeIterator< T > prev() const;
+
+  const T & data() const;
+};
+
 int main()
 {
   //в твоём сообщении много букв
