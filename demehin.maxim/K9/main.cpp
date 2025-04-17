26a6ef1076dd685b23aef5185e4e54d632034435
TvoyBatyA12343 <maxdem06@gmail.com>
2025-04-17 18:10:45 +0300

    omg gg lmao ...

--- demehin.maxim/K9/main.cpp
+++ demehin.maxim/K9/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <utility>
+#include <functional>
+#include <string>
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 struct TriTree
 {
   std::pair< T, T > data;
-  TriTree< T, Cmp >* left, * middle, * right, * parent;
+  TriTree< T, Cmp >* left;
+  TriTree< T, Cmp >* middle;
+  TriTree< T, Cmp >* right;
+  TriTree< T, Cmp >* parent;
 };
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 struct TriTreeIterator
 {
   using this_t = TriTreeIterator< T, Cmp >;
@@ -15,79 +21,19 @@ struct TriTreeIterator
   TriTree< T, Cmp >* current;
   bool isReversed;
 
-  explicit TriTreeIterator(TriTree< T, Cmp >* root, bool rev = false):
+  explicit TriTreeIterator(TriTree< T, Cmp >* root, bool rev = false) :
     current(root),
     isReversed(rev)
   {}
 
   bool hasNext() const
   {
-    if (current == nullptr)
-    {
-      return false;
-    }
-
-    if (!isReversed)
-    {
-      if (current->left != nullptr || current->middle != nullptr || current->right != nullptr)
-      {
-        return true;
-      }
-
-      TriTree< T, Cmp >* node = current;
-      while (node->parent != nullptr)
-      {
-        if (node == node->parent->left && node->parent->middle != nullptr)
-        {
-          return true;
-        }
-        if (node == node->parent->middle && node->parent->right != nullptr)
-        {
-          return true;
-        }
-        node = node->parent;
-      }
-      return false;
-    }
-    else
-    {
-      return hasPrev();
-    }
+    return current != nullptr;
   }
 
   bool hasPrev() const
   {
-    if (current == nullptr)
-    {
-      return false;
-    }
-
-    if (isReversed)
-    {
-      if (current->right != nullptr || current->middle != nullptr || current->left != nullptr)
-      {
-        return true;
-      }
-
-      TriTree< T, Cmp >* node = current;
-      while (node->parent != nullptr)
-      {
-        if (node == node->parent->right && node->parent->middle != nullptr)
-        {
-          return true;
-        }
-        if (node == node->parent->middle && node->parent->left)
-        {
-          return true;
-        }
-        node = node->parent;
-      }
-      return false;
-    }
-    else
-    {
-      return hasNext();
-    }
+    return current != nullptr;
   }
 
   this_t next() const
@@ -96,41 +42,7 @@ struct TriTreeIterator
     {
       return *this;
     }
-
-    if (!isReversed)
-    {
-      if (current->left != nullptr)
-      {
-        return this_t(current->left, isReversed);
-      }
-      if (current->middle != nullptr)
-      {
-        return this_t(current->middle, isReversed);
-      }
-      if (current->right != nullptr)
-      {
-        return this_t(current->right, isReversed);
-      }
-
-      TriTree< T, Cmp >* node = current;
-      while (node->parent != nullptr)
-      {
-        if (node == node->parent->left && node->parent->middle != nullptr)
-        {
-          return this_t(node->parent->middle, isReversed);
-        }
-        if (node == node->parent->middle && node->parent->right != nullptr)
-        {
-          return this_t(node->parent->right, isReversed);
-        }
-        node = node->parent;
-      }
-      return this_t(nullptr, isReversed);
-    }
-    else
-    {
-      return prev();
-    }
+    return this_t(current->right, isReversed);
   }
 
   this_t prev() const
@@ -139,41 +51,7 @@ struct TriTreeIterator
     {
       return *this;
     }
-
-    if (isReversed)
-    {
-      if (current->right != nullptr)
-      {
-        return this_t(current->right, isReversed);
-      }
-      if (current->middle != nullptr)
-      {
-        return this_t(current->middle, isReversed);
-      }
-      if (current->left != nullptr)
-      {
-        return this_t(current->left, isReversed);
-      }
-
-      TriTree< T, Cmp >* node = current;
-      while (node->parent != nullptr)
-      {
-        if (node == node->parent->right && node->parent->middle != nullptr)
-        {
-          return this_t(node->parent->middle, isReversed);
-        }
-        if (node == node->parent->middle && node->parent->left != nullptr)
-        {
-          return this_t(node->parent->left, isReversed);
-        }
-        node = node->parent;
-      }
-      return this_t(nullptr, isReversed);
-    }
-    else
-    {
-      return next();
-    }
+    return this_t(current->left, isReversed);
   }
 
   std::pair< T, T >& data()
@@ -182,45 +60,25 @@ struct TriTreeIterator
   }
 };
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 TriTreeIterator< T, Cmp > begin(TriTree< T, Cmp >* root)
 {
-  if (root == nullptr)
-  {
-    return TriTreeIterator< T, Cmp >(nullptr);
-  }
-  TriTree< T, Cmp >* node = root;
-  while (node->left != nullptr)
-  {
-    node = node->left;
-  }
-  return TriTreeIterator< T, Cmp >(node);
+  return TriTreeIterator< T, Cmp >(root);
 }
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 TriTreeIterator< T, Cmp > rbegin(TriTree< T, Cmp >* root)
 {
-  if (root == nullptr)
-  {
-    return TriTreeIterator< T, Cmp >(nullptr);
-  }
-
-  TriTree< T, Cmp >* node = root;
-  while (node->right != nullptr)
-  {
-    node = node->right;
-  }
-  return TriTreeIterator< T, Cmp >(node, true);
+  return TriTreeIterator< T, Cmp >(root, true);
 }
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 bool canBeInserted(TriTree< T, Cmp >* node, const std::pair< T, T >& pair, Cmp cmp = Cmp())
 {
   if (node == nullptr)
   {
     return true;
   }
-
   if (cmp(pair.first, node->data.first) && cmp(pair.second, node->data.first))
   {
     return canBeInserted(node->left, pair, cmp);
@@ -236,8 +94,8 @@ bool canBeInserted(TriTree< T, Cmp >* node, const std::pair< T, T >& pair, Cmp c
   return false;
 }
 
-template< class T, class Cmp >
-bool insertPair(TriTree< T, Cmp >** node, const std::pair< T, T >& pair, TriTree< T, Cmp >* parent = nullptr, Cmp cmp = Cmp())
+template < class T, class Cmp >
+bool insertPair(TriTree< T, Cmp >** node, const std::pair< T, T >& pair, TriTree< T, Cmp >* parent = nullptr)
 {
   if (*node == nullptr)
   {
@@ -245,22 +103,23 @@ bool insertPair(TriTree< T, Cmp >** node, const std::pair< T, T >& pair, TriTree
     return true;
   }
 
+  Cmp cmp;
   if (cmp(pair.first, (*node)->data.first) && cmp(pair.second, (*node)->data.first))
   {
-    return insertPair(&(*node)->left, pair, *node, cmp);
+    return insertPair(&(*node)->left, pair, *node);
   }
   else if (cmp((*node)->data.second, pair.first) && cmp((*node)->data.second, pair.second))
   {
-    return insertPair(&(*node)->right, pair, *node, cmp);
+    return insertPair(&(*node)->right, pair, *node);
   }
   else if (!cmp(pair.first, (*node)->data.first) && !cmp((*node)->data.second, pair.second))
   {
-    return insertPair(&(*node)->middle, pair, *node, cmp);
+    return insertPair(&(*node)->middle, pair, *node);
   }
   return false;
 }
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 void clear(TriTree< T, Cmp >* node)
 {
   if (node != nullptr)
@@ -272,89 +131,46 @@ void clear(TriTree< T, Cmp >* node)
   }
 }
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 size_t countIntersects(TriTree< T, Cmp >* root, const T& v1, const T& v2, Cmp cmp = Cmp())
 {
   size_t cnt = 0;
-  if (cmp(v2, v1))
+  for (auto it = begin(root); it.hasNext(); it = it.next())
   {
-    for (auto it = rbegin(root); it.hasPrev(); it = it.prev())
+    const auto& pair = it.data();
+    if (!(cmp(pair.second, v1) || cmp(v2, pair.first)))
     {
-      const auto& pair = it.data();
-      if (!(cmp(pair.second, v2) || cmp(v1, pair.first)))
-      {
-        cnt++;
-      }
+      cnt++;
     }
   }
-  else
-  {
-    for (auto it = begin(root); it.hasNext(); it = it.next())
-    {
-      const auto& pair = it.data();
-      if (!(cmp(pair.second, v1) || cmp(v2, pair.first)))
-      {
-        cnt++;
-      }
-    }
-  }
-
   return cnt;
 }
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 size_t countCovers(TriTree< T, Cmp >* root, const T& v1, const T& v2, Cmp cmp = Cmp())
 {
   size_t cnt = 0;
-  if (cmp(v2, v1))
+  for (auto it = begin(root); it.hasNext(); it = it.next())
   {
-    for (auto it = rbegin(root); it.hasPrev(); it = it.prev())
+    const auto& pair = it.data();
+    if (!cmp(v1, pair.first) && !cmp(pair.second, v2))
     {
-      const auto& pair = it.data();
-      if (!cmp(v2, pair.first) && !cmp(pair.second, v1))
-      {
-        cnt++;
-      }
-    }
-  }
-  else
-  {
-    for (auto it = begin(root); it.hasNext(); it = it.next())
-    {
-      const auto& pair = it.data();
-      if (!cmp(v1, pair.first) && !cmp(pair.second, v2))
-      {
-        cnt++;
-      }
+      cnt++;
     }
   }
   return cnt;
 }
 
-template< class T, class Cmp >
+template < class T, class Cmp >
 size_t countAvoids(TriTree< T, Cmp >* root, const T& v1, const T& v2, Cmp cmp = Cmp())
 {
   size_t cnt = 0;
-  if (cmp(v2, v1))
+  for (auto it = begin(root); it.hasNext(); it = it.next())
   {
-    for (auto it = rbegin(root); it.hasPrev(); it = it.prev())
+    const auto& pair = it.data();
+    if (cmp(pair.second, v1) || cmp(v2, pair.first))
     {
-      const auto& pair = it.data();
-      if (cmp(pair.second, v2) || cmp(v1, pair.first))
-      {
-        cnt++;
-      }
-    }
-  }
-  else
-  {
-    for (auto it = begin(root); it.hasNext(); it = it.next())
-    {
-      const auto& pair = it.data();
-      if (cmp(pair.second, v1) || cmp(v2, pair.first))
-      {
-        cnt++;
-      }
+      cnt++;
     }
   }
   return cnt;
@@ -381,17 +197,29 @@ int main()
       clear(root);
       return 1;
     }
+
     if (a == b)
     {
-      continue;
+      std::cerr << "<INVALID COMMAND>\n";
+      clear(root);
+      return 1;
     }
 
-    std::pair< int, int > pair = cmp(a, b) ? std::make_pair(a, b) : std::make_pair(b, a);
+    if (cmp(b, a))
+    {
+      std::cerr << "<INVALID COMMAND>\n";
+      clear(root);
+      return 1;
+    }
+
+    std::pair< int, int > pair(a, b);
+
     if (root == nullptr)
     {
       root = new TriTree< int, std::less< int > >{ pair, nullptr, nullptr, nullptr, nullptr };
       continue;
     }
+
     if (canBeInserted(root, pair, cmp))
     {
       insertPair(&root, pair);
@@ -403,24 +231,30 @@ int main()
   {
     if (command == "intersects" || command == "covers" || command == "avoids")
     {
-      int v1;
-      int v2;
+      int v1, v2;
       if (!(std::cin >> v1 >> v2))
       {
         std::cerr << "<INVALID COMMAND>\n";
         continue;
       }
+
+      if (cmp(v2, v1))
+      {
+        std::cerr << "<INVALID COMMAND>\n";
+        continue;
+      }
+
       if (command == "intersects")
       {
-        std::cout << countIntersects(root, v1, v2, cmp) << "\n";
+        std::cout << countIntersects(root, v1, v2) << "\n";
       }
       else if (command == "covers")
       {
-        std::cout << countCovers(root, v1, v2, cmp) << "\n";
+        std::cout << countCovers(root, v1, v2) << "\n";
       }
       else if (command == "avoids")
       {
-        std::cout << countAvoids(root, v1, v2, cmp) << "\n";
+        std::cout << countAvoids(root, v1, v2) << "\n";
       }
     }
     else
@@ -432,5 +266,5 @@ int main()
   }
 
   clear(root);
+  return 0;
 }
-
